db1a8f20f3c0efa61575261b91dc17a3644581c4
a person <suyashtnt@gmail.com>
2020-09-04 17:00:16 +0200

    Add Amazing People.svelte

---
+++ src/routes/Amazing People.svelte
@@ -0,0 +1,12 @@
+<h1> All the people who helped make this site and others</h1>
+
+<ul>
+    <li>Rich Harris - Making svelte</li>
+    <li>Evan You - creating vue</li>
+    <li>CEO of discord - for creating discord</li>
+    <li>Sebs - help debug moosik and this site</li>
+    <li>Charlie - help debug the css and design ideas for this site</li>
+    <li>m1sf3t - help with animation debugging</li>
+    <li>Adam Wathan - creating tailwind</li>
+    <li>Many, Many more that are deep into discord servers</li>
+</ul>
